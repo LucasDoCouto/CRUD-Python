d87e2ead56638080d4965d6e876728aec28c352c
Lucas do Couto <48269935+LucasDoCouto@users.noreply.github.com>
2023-09-11 20:14:21 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
+# Importa a blibioteca do MySQL
 import mysql.connector
 
+# Faz a conexão com o BD
 conexao = mysql.connector.connect(
     host='localhost',
     user='root',
-    password='1234',
+    password='',
     database='teste'
 )
 
 cursor = conexao.cursor()
+
+# CRUD
+
+# Cria tabela
+criaTabela = 'CREATE TABLE PYTHON (coluna char);'
+cursor.execute(criaTabela)
+conexao.commit()
+
+# Finaliza a conexão do BD
+cursor.close()
+conexao.close()
